3a4d63016a365a7aac3e40a4e765bd5adc4e426b
Wesley Camargo <camargo.wes@gmail.com>
2018-12-09 12:41:29 -0200

    implementing backup

--- Tasks/WebAppBackup/backupWebApp.ps1
+++ Tasks/WebAppBackup/backupWebApp.ps1
@@ -16,101 +16,72 @@ $containerName = Get-VstsInput -Name containerName
 $webappname = Get-VstsInput -Name webappname
 $webAppResourceGroupName = Get-VstsInput -Name webAppResourceGroupName
 $restore = Get-VstsInput -Name restore
-
+$restoreEnvironment = Get-VstsInput -Name restoreEnvironment
 
 
 #formata o nome do arquivo e diretorios
 $releaseId = $env:RELEASE_RELEASENAME
-$environmentName = $env:RELEASE_ENVIRONMENTNAME
-$backupname = "$webappname/$releaseId/$environmentName/$releaseId"
-
-
-
-# $serviceNameInput = Get-VstsInput -Name ConnectedServiceNameSelector -Default 'ConnectedServiceName'
-# $serviceName = Get-VstsInput -Name $serviceNameInput -Default (Get-VstsInput -Name DeploymentEnvironmentName)
-# if (!$serviceName) {
-#     Get-VstsInput -Name $serviceNameInput -Require
-# }
-
-    
-
-# Write-Output "service endpoint: $serviceNameInput"
-# $vstsEndpoint = Get-VstsEndpoint -Name $serviceNameInput -Require
-# # if ($vstsEndpoint.Auth.Scheme -ne 'ServicePrincipal') {
-# #     throw "$($vstsEndpoint.Auth.Scheme) endpoint not supported"
-# # }
 
 Write-Output "Getting vsts endpoint..."
 $serviceNameInput = Get-VstsInput -Name ConnectedServiceNameSelector -Default 'ConnectedServiceName'
- Write-Host $serviceNameInput
- $serviceName = Get-VstsInput -Name $serviceNameInput -Default (Get-VstsInput -Name DeploymentEnvironmentName)
+$serviceName = Get-VstsInput -Name $serviceNameInput -Default (Get-VstsInput -Name DeploymentEnvironmentName)
 
- Write-Host $serviceName
-        if (!$serviceName) {
-            # Let the task SDK throw an error message if the input isn't defined.
-            Get-VstsInput -Name $serviceNameInput -Require
-        }
+Write-Host $serviceName
+if (!$serviceName) {
+    Get-VstsInput -Name $serviceNameInput -Require
+}
 
-        $vstsEndpoint = Get-VstsEndpoint -Name $serviceName -Require
-
-        Write-Host "TenantId: $($vstsEndpoint.Auth.Parameters.TenantId)"
+$vstsEndpoint = Get-VstsEndpoint -Name $serviceName -Require
 
 $cred = New-Object System.Management.Automation.PSCredential(
     $vstsEndpoint.Auth.Parameters.ServicePrincipalId,
     (ConvertTo-SecureString $vstsEndpoint.Auth.Parameters.ServicePrincipalKey -AsPlainText -Force))
+
 Login-AzureRmAccount -Credential $cred -ServicePrincipal -TenantId $vstsEndpoint.Auth.Parameters.TenantId -SubscriptionId $vstsEndpoint.Data.SubscriptionId
-
-Write-Output Get-AzureRmResourceGroup
-
     
-Write-Output "Obtendo Storage Account..."
+Write-Output "Getting Storage Account..."
 $storage = Get-AzureRmStorageAccount -ResourceGroupName $storageAccountResourceGroupName -Name $storagename
        
 if (-Not ((Get-AzureStorageContainer -Context $storage.Context).Name -contains $containerName)) {
-    Write-Output "Criando o storage container"
+    Write-Output "Creating storage container"
     New-AzureStorageContainer -Name $containerName -Context $storage.Context
-    # $container = Get-AzureRmStorageContainer -ResourceGroupName $storageAccountResourceGroupName -Name $containerName -StorageAccountName $storagename
 }
 
-Write-Output "Criando SAS para o container"
+Write-Output "Creating SAS to container"
 $sasUrl = New-AzureStorageContainerSASToken -Name $containerName -Permission rwdl `
     -Context $storage.Context -ExpiryTime (Get-Date).AddMonths(1) -FullUri
 
 Write-Output "Restore: $restore"
 
+if ($restore  -eq "true") {    
+    Write-Output "Initializing restore from '$restoreEnvironment' environment ..."
 
-
-# if ($restore) {    
-#     Write-Output "Iniciando restore..."
-#     $backup = Get-AzureRmWebAppBackupList -ResourceGroupName $webAppResourceGroupName -Name $webappname `
-#         | where { $_.BackupName -contains $backupname } `
-#         | Select-Object -Last 1 | sort { $_.BackupId } -Descending
+    $backupname = "$webappname/$releaseId/$restoreEnvironment/$releaseId"
     
-#     Write-Output "Backup"
-#     Write-Output $backup
+    $backup = Get-AzureRmWebAppBackupList -ResourceGroupName $webAppResourceGroupName -Name $webappname `
+        | where { $_.BackupName -contains $backupname } `
+        | Select-Object -Last 1 | sort { $_.BackupId } -Descending
+    
+    Write-Output "Backup"
+    Write-Output $backup
 
-#     $backup | Restore-AzureRmWebAppBackup -Overwrite
+    $backup | Restore-AzureRmWebAppBackup -Overwrite
 
-#     return
-# }
+    return
+}
 
+$environmentName = $env:RELEASE_ENVIRONMENTNAME
+$backupname = "$webappname/$releaseId/$environmentName/$releaseId"
 
-Write-Output "Iniciando backup...."
+Write-Output "Initializing backup...."
 $backup = New-AzureRmWebAppBackup -ResourceGroupName $webAppResourceGroupName -Name $webappname `
     -StorageAccountUrl $sasUrl -BackupName $backupname 
     
 do {
-    Write-Output "Status do backup...."
+    Write-Output "Backup Status...."
     $statusBackup = Get-AzureRmWebAppBackup -ResourceGroupName $webAppResourceGroupName -Name $webappname -BackupId $backup.BackupId
 
     Write-Output $statusBackup 
 
     Start-Sleep -s 10
 }while ($statusBackup.BackupStatus -eq "InProgress") 
-        
-# }
-# catch {
-#     Write-Error "Ocorreu um erro ao realizar o backup:"
-#     Write-Error $_.Exception.Message
-# }
-

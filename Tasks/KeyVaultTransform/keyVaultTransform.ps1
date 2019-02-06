48b3779c8e065e17d0beeb4de9e2c2f290e3dff7
Wesley Camargo <wesley.camargo@esx.com.br>
2019-02-06 16:12:50 -0200

    suporte a transformacoes fora do zip

--- Tasks/KeyVaultTransform/keyVaultTransform.ps1
+++ Tasks/KeyVaultTransform/keyVaultTransform.ps1
@@ -18,7 +18,7 @@ try {
     AzureLogin
 
     $PathToArchives = Get-VstsInput -Name PathToArchives -Require
-    $Packages = Get-VstsInput -Name Packages -Require
+    $Packages = Get-VstsInput -Name Packages 
     $FilesToTokenize = Get-VstsInput -Name FilesToTokenize -Require
     $Prefix = Get-VstsInput -Name Prefix
     $Suffix = Get-VstsInput -Name Suffix
@@ -55,7 +55,7 @@ try {
 
     $ErrorActionPreference = "Stop"
 
-    $zips = Get-ChildItem -Path $PathToArchives -Include $Packages -Recurse
+    
 
     $replaceWithEmptyText = $False
     if ($ReplaceWithEmpty.ToLowerInvariant() -eq "true") {
@@ -66,104 +66,163 @@ try {
     if ($ReplaceTokenFile.ToLowerInvariant() -eq "true") {
         $ReplaceTokenFileText = $True
     }
-    
 
-    # Load ZipFile (Compression.FileSystem) if necessary
-    try { $null = [IO.Compression.ZipFile] }
-    catch { [System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem') }
+    if ([string]::IsNullOrEmpty($Packages)) {
+        Write-Host "Arquivo zip nao informado"
+        $matchingFiles = Get-ChildItem -Path $PathToArchives -Recurse | Where-Object { $_.FullName -match "$FilesToTokenize" }
+        
+        Write-Host "Arquivos: $matchingFiles"
+        
+        $matchingFiles | ForEach-Object {
+            $matchingEntry = $_
+            Write-Host "Matched file in zip: $($_.FullName)"
+            # If needed, read the contents of specific file to $text and release the file so to use streamwriter later
+                                    
+            $content = Get-Content $matchingEntry.FullName
+            
+            $replaceCallback = {
+                param(
+                    [System.Text.RegularExpressions.Match] $Match
+                )
 
-    $zips | ForEach-Object {
-        $file = $_
-        Write-Host "Matched zip: $($_.FullName)"
-        # Open zip file with update mode (Update, Read, Create -- are the options)
-        try { 
-            $fileZip = [System.IO.Compression.ZipFile]::Open( $file, 'Update' ) 
+                Write-Host "Match: $Match"
+                    
+                # $value = Get-TaskVariable -Name $Match.Groups[1].Value                  
+                $keyVaultNames = SplitKeyVaults -valor $keyVaults -separador ","
+                
+                $key = $($Match.Groups[1].Value)
+
+                $value = Get-KeyVaultSecret -SecretName $key -KeyVaultNames $keyVaultNames
+
+                Write-Host "Finding key: $key"
+                Write-Host "KeyVault Value: $value"
+
+                # $value = Get-TaskVariable -Name $Match.Groups[1].Value
+                if (!$value) {
+                    if ($replaceWithEmptyText) {
+                        $value = [string]::Empty
+                        Write-Verbose "Variable '$($Match.Groups[1].Value)' not found. Replaced with empty." -Verbose
+                    }
+                    else {
+                        $value = $Match.Value
+                        Write-Verbose "Variable '$($Match.Groups[1].Value)' not found. Kept token." -Verbose
+                    }
+                }
+                    
+                Write-Verbose "Replacing '$($Match.Value)' with '${value}'" -Verbose
+                $value
+            }      
+
+            $content = $regex.Replace($content, $replaceCallback)
+            # Re-open the file this time with streamwriter                       
+          
+            
+            Write-Host "Saving content file $matchingEntry"
+
+            Set-Content $matchingEntry.FullName $content
         }
-        catch { throw "Another process has locked the '$file' file." }
+    }
+    else {
+        Write-Host "Arquivo zip: $Packages"    
+    
+        $zips = Get-ChildItem -Path $PathToArchives -Include $Packages -Recurse
 
-        # Finding the specific file within the zip file
-        <#
+        # Load ZipFile (Compression.FileSystem) if necessary
+        try { $null = [IO.Compression.ZipFile] }
+        catch { [System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem') }
+
+        $zips | ForEach-Object {
+            $file = $_
+            Write-Host "Matched zip: $($_.FullName)"
+            # Open zip file with update mode (Update, Read, Create -- are the options)
+            try { 
+                $fileZip = [System.IO.Compression.ZipFile]::Open( $file, 'Update' ) 
+            }
+            catch { throw "Another process has locked the '$file' file." }
+
+            # Finding the specific file within the zip file
+            <#
         NOTE: These entries have the directories separated with a forward slash (/) instead of MS convention
         of a backward slash (\).  Even though this is regex it seems the forward slash does not need to be escaped (\/).
         NOTE2: Because this is regex '.*' must be used instead of a simple '*'
         #>
-        try {
-            $matchingFiles = $fileZip.Entries | Where-Object { $_.FullName -match "$FilesToTokenize" }
+            try {
+                $matchingFiles = $fileZip.Entries | Where-Object { $_.FullName -match "$FilesToTokenize" }
 
-            $matchingFiles | ForEach-Object {
-                $matchingEntry = $_
-                Write-Host "Matched file in zip: $($_.FullName)"
-                # If needed, read the contents of specific file to $text and release the file so to use streamwriter later
-                $desiredFile = [System.IO.StreamReader]($matchingEntry).Open()
-                $content = $desiredFile.ReadToEnd()
-                $desiredFile.Close()
-                $desiredFile.Dispose()
+                $matchingFiles | ForEach-Object {
+                    $matchingEntry = $_
+                    Write-Host "Matched file in zip: $($_.FullName)"
+                    # If needed, read the contents of specific file to $text and release the file so to use streamwriter later
+                    $desiredFile = [System.IO.StreamReader]($matchingEntry).Open()
+                    $content = $desiredFile.ReadToEnd()
+                    $desiredFile.Close()
+                    $desiredFile.Dispose()
 
-                $replaceCallback = {
-                    param(
-                        [System.Text.RegularExpressions.Match] $Match
-                    )
+                    $replaceCallback = {
+                        param(
+                            [System.Text.RegularExpressions.Match] $Match
+                        )
                     
-                    # $value = Get-TaskVariable -Name $Match.Groups[1].Value                  
-                    $keyVaultNames = SplitKeyVaults -valor $keyVaults -separador ","
+                        # $value = Get-TaskVariable -Name $Match.Groups[1].Value                  
+                        $keyVaultNames = SplitKeyVaults -valor $keyVaults -separador ","
                     
-                    $value = Get-KeyVaultSecret -SecretName $Match.Groups[1].Value -KeyVaultNames $keyVaultNames
+                        $value = Get-KeyVaultSecret -SecretName $Match.Groups[1].Value -KeyVaultNames $keyVaultNames
            
-                    Write-Host "KeyVault Value: $value"
+                        Write-Host "KeyVault Value: $value"
 
-                    # $value = Get-TaskVariable -Name $Match.Groups[1].Value
-                    if (!$value)
-                    {
-                        if ($replaceWithEmptyText) {
-                            $value = [string]::Empty
-                            Write-Verbose "Variable '$($Match.Groups[1].Value)' not found. Replaced with empty." -Verbose
+                        # $value = Get-TaskVariable -Name $Match.Groups[1].Value
+                        if (!$value) {
+                            if ($replaceWithEmptyText) {
+                                $value = [string]::Empty
+                                Write-Verbose "Variable '$($Match.Groups[1].Value)' not found. Replaced with empty." -Verbose
+                            }
+                            else {
+                                $value = $Match.Value
+                                Write-Verbose "Variable '$($Match.Groups[1].Value)' not found. Kept token." -Verbose
+                            }
                         }
-                        else {
-                            $value = $Match.Value
-                            Write-Verbose "Variable '$($Match.Groups[1].Value)' not found. Kept token." -Verbose
-                        }
+                    
+                        Write-Verbose "Replacing '$($Match.Value)' with '${value}'" -Verbose
+                        $value
                     }
-                    
-                    Write-Verbose "Replacing '$($Match.Value)' with '${value}'" -Verbose
-                    $value
-                }
         
-                $content = $regex.Replace($content, $replaceCallback)
-                # Re-open the file this time with streamwriter
+                    $content = $regex.Replace($content, $replaceCallback)
+                    # Re-open the file this time with streamwriter
                         
-                if($ReplaceTokenFileText)
-                {   
-                    Write-Host "Opening file $TargetTokenFile"                  
-                    $targetFile = $fileZip.Entries | Where-Object  { $_.FullName -match "$TargetTokenFile" }                    
-                    $desiredFile = [System.IO.StreamWriter]($targetFile).Open()   
+                    if ($ReplaceTokenFileText) {   
+                        Write-Host "Opening file $TargetTokenFile"                  
+                        $targetFile = $fileZip.Entries | Where-Object { $_.FullName -match "$TargetTokenFile" }                    
+                        $desiredFile = [System.IO.StreamWriter]($targetFile).Open()   
                     
-                    Write-Host "Removing token file $matchingEntry"
-                    [System.IO.StreamReader]($matchingEntry).Delete()
+                        Write-Host "Removing token file $matchingEntry"
+                        [System.IO.StreamReader]($matchingEntry).Delete()
 
-                }else{
-                    Write-Host "Opening file $matchingEntry"
-                    $desiredFile = [System.IO.StreamWriter]($matchingEntry).Open()
+                    }
+                    else {
+                        Write-Host "Opening file $matchingEntry"
+                        $desiredFile = [System.IO.StreamWriter]($matchingEntry).Open()
+                    }
+
+                    # If needed, zero out the file -- in case the new file is shorter than the old one
+                    $desiredFile.BaseStream.SetLength(0)
+
+                    # Insert the $text to the file and close
+                    $desiredFile.Write($content -join "`r`n")
+                    $desiredFile.Flush()
+                    $desiredFile.Close()
                 }
-
-                # If needed, zero out the file -- in case the new file is shorter than the old one
-                $desiredFile.BaseStream.SetLength(0)
-
-                # Insert the $text to the file and close
-                $desiredFile.Write($content -join "`r`n")
-                $desiredFile.Flush()
-                $desiredFile.Close()
-            }
             
-            # Write the changes and close the zip file
-            $fileZip.Dispose()
-        }
-        finally
-        {
-            $fileZip.Dispose()
-        }
+                # Write the changes and close the zip file
+                $fileZip.Dispose()
+            }
+            finally {
+                $fileZip.Dispose()
+            }
 
         
+        }
     }
+    
 }
 catch [Exception] {
     Write-Error ($_.Exception.ToString())
@@ -171,5 +230,5 @@ catch [Exception] {
     Write-Host "##vso[task.complete result=Failed;]Unintentional failure. Error encountered. Defaulting to always fail." 
 } 
 finally {
-	Trace-VstsLeavingInvocation $MyInvocation
+    Trace-VstsLeavingInvocation $MyInvocation
 }
